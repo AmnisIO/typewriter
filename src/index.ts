dad7826bce0e5c45e9b598636fe10e8046628357
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-26 04:20:02 +0530

    Use typewrite function

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
 import { getAST } from './ast';
 import { emit } from './emitter';
+import * as fs from 'fs';
 
-declare function require(name: string);
+declare function require(name: string): any;
 declare var process: any;
 const util = require('util');
 
-(function () {
-  if (typeof process !== 'undefined' && process.nextTick && !process.browser && typeof require !== "undefined") {
-    var fs = require('fs');
-    if (process.argv.length < 2)
-      process.exit();
-    var fileNames = process.argv.slice(2);
-    const sourceFile = getAST(fileNames);
-    const result = emit(sourceFile, { include_headers: '' });
-    process.stdout.write(util.format.apply(null, [result.emitted_string]) + '\n');
-    process.exit();
-  }
-})();
+export const typewrite = (file: string, output: string): void => {
+  const sourceFile = getAST([file]);
+  const result = emit(sourceFile, { include_headers: '' });
+  fs.writeFileSync(output, result.emitted_string);
+};
+
+export default typewrite;
