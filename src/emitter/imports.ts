e3f4419b1913417c6cdcb51552e031fe1690f8b6
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-25 15:31:42 +0530

    Refine imports area of transpilation

--- src/emitter/imports.ts
+++ src/emitter/imports.ts
@@ -1,18 +1,22 @@
 import { ImportDeclaration, NamedImports, StringLiteral } from 'typescript';
 import { Context, getContext } from '../contexts';
 import { EmitResult } from './';
-// import { titleCase } from 'change-case';
 
 export const emitImportDeclaration = (node: ImportDeclaration, context: Context): EmitResult => {
   const module_specifier = (node.moduleSpecifier as StringLiteral).text.split('/').pop();
-  // const module_header = titleCase(module_specifier);
   const module_context = getContext(module_specifier);
-  const include_headers = module_context.include_headers.split(',').filter(x => !!x).map(header => `#include <${header}.h>\n`);
+  const emitted_string =
+    (module_context.include_headers || '')
+      .split(',')
+      .map(header => header.trim())
+      .filter(x => !!x)
+      .map(header => `#include <${header}.h>`)
+      .join('\n');
   return {
     context: {
       ...context,
       ...module_context
     },
-    emitted_string: `${include_headers}#include <Gyrus.h>`
+    emitted_string
   };
 }
