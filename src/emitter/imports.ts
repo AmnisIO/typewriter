3445da2647d8b4e5c5bc283c2d3e54a3363c8ce0
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-25 14:48:38 +0530

    Add includes from context

--- src/emitter/imports.ts
+++ src/emitter/imports.ts
@@ -5,13 +5,14 @@ import { titleCase } from 'change-case';
 
 export const emitImportDeclaration = (node: ImportDeclaration, context: Context): EmitResult => {
   const module_specifier = (node.moduleSpecifier as StringLiteral).text.split('/').pop();
-  const module_header = titleCase(module_specifier);
+  // const module_header = titleCase(module_specifier);
   const module_context = getContext(module_specifier);
+  const include_headers = module_context.include_headers.split(',').filter(x => !!x).map(header => `#include <${header}.h>\n`);
   return {
     context: {
       ...context,
       ...module_context
     },
-    emitted_string: `#include "Gyrus.h" // Gyrus for ${module_header}`
+    emitted_string: `${include_headers}#include <Gyrus.h>`
   };
 }
