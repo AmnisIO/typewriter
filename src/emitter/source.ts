147e5a5c6b4281224813b424e53fcf9dfebe6061
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-25 15:31:59 +0530

    Emit only if the node emission is non empty

--- src/emitter/source.ts
+++ src/emitter/source.ts
@@ -6,6 +6,7 @@ export const emitSourceFile = (node: SourceFile, context: Context): EmitResult =
   node.statements
     .reduce<EmitResult>(({ context, emitted_string }, node) => {
       const result = emit(node, context);
+      if (!result.emitted_string) return result;
       result.emitted_string = emitted_string + '\n\n' + result.emitted_string;
       return result;
     }, { context, emitted_string: '' });
