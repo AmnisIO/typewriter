993c883ea9d4940a5b844bb4d3edc0ea7180ec52
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-25 11:45:22 +0530

    Update import

--- src/emitter/imports.ts
+++ src/emitter/imports.ts
@@ -1,17 +1,17 @@
 import { ImportDeclaration, NamedImports, StringLiteral } from 'typescript';
 import { Context, getContext } from '../contexts';
 import { EmitResult } from './';
-import { pascalCase } from 'change-case';
+import { titleCase } from 'change-case';
 
 export const emitImportDeclaration = (node: ImportDeclaration, context: Context): EmitResult => {
   const module_specifier = (node.moduleSpecifier as StringLiteral).text.split('/').pop();
-  const module_header = pascalCase(module_specifier.split('-').join(' '));
+  const module_header = titleCase(module_specifier);
   const module_context = getContext(module_specifier);
   return {
     context: {
       ...context,
       ...module_context
     },
-    emitted_string: `#include "Gyrus${module_header}.h"`
+    emitted_string: `#include "Gyrus.h" // Gyrus for ${module_header}`
   };
 }
