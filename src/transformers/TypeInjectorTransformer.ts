56527e817abebca7ade2241a806e0ca4dd8cecdd
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-01 20:50:15 +0530

    process function-like declarations differently

--- src/transformers/TypeInjectorTransformer.ts
+++ src/transformers/TypeInjectorTransformer.ts
@@ -1,5 +1,6 @@
-import { transform as transformInternal, TransformerFactory, SourceFile, TypeChecker, visitEachChild, visitNode, Node, SyntaxKind, VariableDeclaration, createTypeReferenceNode } from 'typescript';
+import { transform as transformInternal, TransformerFactory, SourceFile, TypeChecker, visitEachChild, visitNode, Node, SyntaxKind, VariableDeclaration, FunctionLikeDeclaration, createTypeReferenceNode } from 'typescript';
 import { Transformer } from './types';
+import { path } from 'ramda';
 
 export class TypeInjectorTransformer implements Transformer {
   private _typeChecker: TypeChecker;
@@ -7,14 +8,35 @@ export class TypeInjectorTransformer implements Transformer {
     this._typeChecker = typeChecker;
   }
   getTransformer(): TransformerFactory<SourceFile> {
+    const typeChecker = this._typeChecker;
+    const getTypeAtLocation = typeChecker.getTypeAtLocation;
+    const toString = typeChecker.typeToString;
     return context => node => {
       const injectTypeIfNeeded = (node: Node): Node => {
-        if (node.kind === SyntaxKind.VariableDeclaration) {
-          const declaration = node as VariableDeclaration;
-          if (declaration.type != undefined) return;
-          declaration.type = createTypeReferenceNode(this._typeChecker.typeToString(this._typeChecker.getTypeAtLocation(node)), []);
+        switch (node.kind) {
+          case SyntaxKind.VariableDeclaration:
+            {
+              const declaration = node as VariableDeclaration;
+              if (declaration.type != undefined) return;
+              const type = getTypeAtLocation(node);
+              const typeString = toString(type);
+              declaration.type = createTypeReferenceNode(typeString, []);
+              return visitEachChild(node, injectTypeIfNeeded, context);
+            }
+          case SyntaxKind.FunctionDeclaration:
+          case SyntaxKind.ArrowFunction:
+            {
+              const declaration = node as FunctionLikeDeclaration;
+              if (declaration.type != undefined) return;
+              const type = getTypeAtLocation(node);
+              const typeString = toString(type);
+              const processedTypeString = typeString.substring(typeString.indexOf("=>") + 2).trim();
+              declaration.type = createTypeReferenceNode(processedTypeString, []);
+              return visitEachChild(node, injectTypeIfNeeded, context);
+            }
+          default:
+            return visitEachChild(node, injectTypeIfNeeded, context);
         }
-        return visitEachChild(node, injectTypeIfNeeded, context);
       };
       return visitNode(node, injectTypeIfNeeded);
     }
