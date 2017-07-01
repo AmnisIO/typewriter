2be31b6dc33b631f091174e001cbe7aeb6d2c809
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-01 21:19:36 +0530

    Export function block transformer

--- src/transformers/index.ts
+++ src/transformers/index.ts
@@ -1,6 +1,7 @@
 import { Transformer } from './types';
 import { transform as transformInternal, SourceFile } from 'typescript';
 export { TypeInjectorTransformer } from './TypeInjectorTransformer';
+export { FunctionBlockTransformer } from './FunctionBlockTransformer';
 
 export const transform = (sourceFile: SourceFile, transformers: Transformer[]) =>
   transformInternal(sourceFile, transformers.map(t => t.getTransformer())).transformed.shift();
