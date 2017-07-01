33c95013bb6b01b60daf21208a07acca5c9d6522
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-01 20:21:23 +0530

    Expose transformers

---
+++ src/transformers/index.ts
@@ -0,0 +1,6 @@
+import { Transformer } from './types';
+import { transform as transformInternal, SourceFile } from 'typescript';
+export { DummyTransformer } from './DummyTransformer';
+
+export const transform = (sourceFile: SourceFile, transformers: Transformer[]) =>
+  transformInternal(sourceFile, transformers.map(t => t.getTransformer())).transformed.shift();
