a725a7443e47c8d6e5e209bd30c5dd080d2bd5c6
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-01 20:22:23 +0530

    Expose TypeInjector transformer

--- src/transformers/index.ts
+++ src/transformers/index.ts
@@ -1,6 +1,7 @@
 import { Transformer } from './types';
 import { transform as transformInternal, SourceFile } from 'typescript';
 export { DummyTransformer } from './DummyTransformer';
+export { TypeInjectorTransformer } from './TypeInjectorTransformer';
 
 export const transform = (sourceFile: SourceFile, transformers: Transformer[]) =>
   transformInternal(sourceFile, transformers.map(t => t.getTransformer())).transformed.shift();
