59a81b120cafc003490720c950a947b857587886
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-02 14:52:50 +0530

    Update test source

--- tests/blink.ts
+++ tests/blink.ts
@@ -1,14 +1,12 @@
 import { ByteStream, Byte, periodic } from '@amnisio/rivulet';
 import { Sources, Sinks, HIGH, LOW, run, createSinks } from '@amnisio/arduino-uno';
 
-const toggle = (value: Byte) => value == HIGH ? LOW : HIGH;
-
 const blink = (arduino: Sources) => {
   const sinks = createSinks();
   sinks.LED$ =
     periodic(1000)
       .sample(arduino.LED$)
-      .map(toggle);
+      .map(x => x == LOW ? HIGH : LOW);
   return sinks;
 }
 
