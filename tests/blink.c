67308c65f094018a0d53282b9fc30c2103f83021
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-02 14:52:56 +0530

    Update test result

--- tests/blink.c
+++ tests/blink.c
@@ -3,15 +3,15 @@
 #include <Arduino.h>
 #include <Gyrus.h>
 
-Byte toggle(Byte value) {
-  return value == HIGH ? LOW : HIGH;
+(x: Byte) => Byte _typewriter_anonymous_1(Byte x) {
+  return x == LOW ? HIGH : LOW;
 }
 
 Sinks* blink(Sources* arduino) {
   Sinks* sinks = sinks_create();
   ByteStream* _typewriter_1 = byte_stream_periodic(1000);
   ByteStream* _typewriter_2 = _typewriter_1->sample(_typewriter_1, arduino->LED);
-  sinks->LED = _typewriter_2->map(_typewriter_2, toggle);
+  sinks->LED = _typewriter_2->map(_typewriter_2, _typewriter_anonymous_1);
   return sinks;
 }
 
