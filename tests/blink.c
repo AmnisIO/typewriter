db55de542a09397b501f5420750d31aed5059419
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-02 13:06:44 +0530

    Update variable names

--- tests/blink.c
+++ tests/blink.c
@@ -9,9 +9,9 @@ Byte toggle(Byte value) {
 
 Sinks* blink(Sources* arduino) {
   Sinks* sinks = sinks_create();
-  ByteStream* ___typewriter_1 = byte_stream_periodic(1000);
-  ByteStream* ___typewriter_2 = ___typewriter_1->sample(___typewriter_1, arduino->LED);
-  sinks->LED = ___typewriter_2->map(___typewriter_2, toggle);
+  ByteStream* _typewriter_1 = byte_stream_periodic(1000);
+  ByteStream* _typewriter_2 = _typewriter_1->sample(_typewriter_1, arduino->LED);
+  sinks->LED = _typewriter_2->map(_typewriter_2, toggle);
   return sinks;
 }
 
