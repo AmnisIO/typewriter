30893a4a9c2e1d0f2a51de78fc979ce1de6def06
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-08 14:27:30 +0530

    Add support for keywords

---
+++ src/emitter/keywords.ts
@@ -0,0 +1,17 @@
+import { SyntaxKind, KeywordTypeNode } from 'typescript';
+import { Context } from '../contexts';
+import { EmitResult, emit } from './';
+
+const keywordsLookup = {
+  [SyntaxKind.NumberKeyword]: 'number',
+  [SyntaxKind.BooleanKeyword]: 'boolean'
+};
+
+export const emitKeyword = ({ kind }: KeywordTypeNode, context: Context): EmitResult => {
+  const text = keywordsLookup[kind];
+  const emitted_string = context[text] == undefined ? text : context[text];
+  return {
+    context,
+    emitted_string
+  };
+};
