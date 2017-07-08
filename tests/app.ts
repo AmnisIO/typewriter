54198ec0c20ce3b689e6d45c7cce734ad7c6f0ab
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-08 13:10:31 +0530

    Rename test files

---
+++ tests/app.ts
@@ -0,0 +1,19 @@
+import { periodic } from '@amnisio/rivulet';
+import { Sources, HIGH, LOW, run, createSinks } from '@amnisio/arduino-uno';
+
+let brightness = 0;
+let change = 5;
+
+const getFadeValue = (led: number) => {
+  brightness = brightness + change;
+  if (brightness <= LOW || brightness >= HIGH) change = -change;
+  return brightness;
+}
+
+function fade(arduino: Sources) {
+  const sinks = createSinks();
+  sinks.D10$ = periodic(30).map(getFadeValue);
+  return sinks;
+}
+
+run(fade);
