f2cbdb294677ca32391e58ee6ffc870b51dd0fff
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-08 14:03:54 +0530

    Update test result

--- tests/app.c
+++ tests/app.c
@@ -7,16 +7,16 @@ int brightness = 0;
 
 int change = 5;
 
-int getFadeValue(ignored_NumberKeyword led) {
+int getBrightness(ignored_NumberKeyword led) {
   brightness = brightness + change;
-  if (brightness ignored_LessThanEqualsToken GYRUS_LOW ignored_BarBarToken brightness ignored_GreaterThanEqualsToken GYRUS_HIGH) change = ignored_PrefixUnaryExpression; 
+  if (brightness <= GYRUS_LOW ignored_BarBarToken brightness >= GYRUS_HIGH) change = ignored_PrefixUnaryExpression; 
   return brightness;
 }
 
 Sinks* fade(Sources* arduino) {
   Sinks* sinks = sinks_create();
   RivuletStream* _typewriter_intermediary_1 = rivulet_stream_periodic(30);
-  sinks->D10 = _typewriter_intermediary_1->map(_typewriter_intermediary_1, getFadeValue);
+  sinks->D10 = _typewriter_intermediary_1->map(_typewriter_intermediary_1, getBrightness);
   return sinks;
 }
 
